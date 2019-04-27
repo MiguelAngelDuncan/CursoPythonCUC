726744607603d222dda8858d1347bf69ea7f816e
MiguelAngelDuncan <mianduncan@gmail.com>
2019-04-27 11:44:43 -0500

    CONDICIONALES

--- codigo1.py
+++ codigo1.py
@@ -10,7 +10,7 @@ y = "texto"  # String
 z = 4.5  # Float
 a = True  # boolean
 b = False # boolean
-c = 4+5j # complejo 
+c = 4 + 5j # complejo 
 print(x, y, z, a, b, c)
 # Conocer el tipo de dato de una variable
 print(type(b))
@@ -36,3 +36,87 @@ x = str("2")
 print(x)
 x = str("2.5")
 print(x)
+
+# manejo de cadenas de texto y alguno metodos
+
+cad = "Hello World"
+print(cad[0])
+print(cad[2])
+print(cad[0:4]) # No toma el ultimo valor
+
+cad = "    Hello World     "
+cad = cad.strip()
+print(cad)
+print(cad[0])
+
+cad = "Hello World"
+print(len(cad))
+print(cad.lower()) # Minuscula
+
+cad = "Hello World"
+print(len(cad))
+print(cad.upper()) # Mayuscula
+
+print(cad.replace('l', 'Y'))
+print(cad.split("l")
+
+# Operaciones
+
+# Operaciones Aritmetica
+a = 2
+b = 3
+c = a + b
+c = a - b 
+c = a * b
+c = a / b # Cociente
+c = a % b # Residuo
+c = a ** b # Exponente
+c = math.sqrt(a) # Raiz
+c = math.pi 
+
+suma = a + b
+print(r)
+
+# Captura por consola
+print ("Digite el nombre: ")
+nombre = input()
+print("Hola "+nombre+"!")
+ 
+print("Digite el numero uno: ")
+n1 = input()
+n1 = float(n1)
+print("Digite el numero dos: ")
+n2 = input()
+n2 = float(n2)
+print(n1+n2)
+
+# Condiciones
+
+a = 2
+b = 5
+if a > b:
+    print (a, "Es mayor que", b)
+else:
+    print (b, "Es mayor que", a)
+    
+if b > a:
+    if b > 1:
+        print (b, "Es mayor 1 y es mayor que",a )
+
+if b > a:
+    if b > 1:
+    print (b, "Es mayor 1 y es mayor que",a )
+
+if a == b:
+    elif a > b:
+    print (a, "Es mayor que", b)
+else:
+    print (b, "Es mayor que", a)
+        
+if a == b and a > 2:
+    print (a, "Es igual a",b,"Mayor que 2") 
+    
+    if a == b or a > 2:
+     print (a, "Es igual a",b,"Mayor que 2") 
+     
+     
