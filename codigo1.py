d2620876c8f99ae33a5f57d6b5d4e322b0ecd458
MiguelAngelDuncan <mianduncan@gmail.com>
2019-04-27 10:42:53 -0500

    tipos de datos

--- codigo1.py
+++ codigo1.py
@@ -1,8 +1,38 @@
-# -*- coding: utf-8 -*-
+# Comentario de linea
 """
-Editor de Spyder
+Comentario de Parrafo
+"""
+# Hola Mundo
+print("Hola Mundo")
+# variables
+x = 4 # int 
+y = "texto"  # String
+z = 4.5  # Float
+a = True  # boolean
+b = False # boolean
+c = 4+5j # complejo 
+print(x, y, z, a, b, c)
+# Conocer el tipo de dato de una variable
+print(type(b))
 
-Este es un archivo temporal.
-"""
-a = 5
-A = 7
+# Conversiones de tipos de datos
+
+# enteros
+x = int(2)
+print(x)
+
+
+x = int("2")
+print(x)
+
+# Float
+x = float("2")
+print(x)
+x = float("2.5")
+print(x)
+
+# String
+x = str("2")
+print(x)
+x = str("2.5")
+print(x)
