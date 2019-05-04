8414102b6dad7f257d3b23ae20c6d3b64469ec1d
Android Sala 15 <android@cuc.edu.co>
2019-05-04 11:27:01 -0500

    while, arrays

--- codigo1.py
+++ codigo1.py
@@ -135,3 +135,119 @@ print('Es mayor de edad') if edad>=18 else print ('Menor de edad')
 
 #Si de Linea sin sino    
 if edad >=18: print("Es mayor de edad")
+
+# arrays
+v = ["Hola", "Mundo", 4, 3.4, ["otro", "Arrays", "Dentro"]]
+v2 = ["Hola", "Mundo"]
+v3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+v4 = range(1,10)
+v5 = range(200)
+
+for x in v5:
+    print(x)
+
+# Acceder al Elemento
+
+print (v2[0])
+
+
+#Modificar el elemento
+
+v2[1] = "Roberto"
+
+#Eliminar elemento
+
+v2.remove("Hola")
+
+#Eliminar el ultimo elemento
+v.pop()
+v.pop(4)
+
+#Agregar elementos
+v.append(5)
+v.insert(1,"Roberto")
+
+#Borrar el Vector
+v.clear()
+
+#Conocer la posición de un elemento
+v.index('Mundo')
+
+#Numero de elementos e un arrays
+len(v)
+# Cuantas veces aparece un elemento en el vector
+v.append('Mundo')
+v.count('Mundo')
+
+
+#Ordenar un vector
+a.sort()
+
+# recorrer con saltos
+# dos en dos
+a= [5, 9, 6, 7, 11]
+for x in a [0:3:2]:
+    print(x)
+
+
+#Preguntas si existe un elemento dentro de un arrays
+a= [5, 9, 6, 7, 11]
+
+20 in 4
+
+res = 20 in a 
+print(res)
+# Acceder al ultimo elemento del vector
+a[-3]
+# Acceder a un elemento de un arrays interno
+#Forma 1
+aux = v[-1]
+aux = aux[0]
+print(aux)
+
+#Forma2
+aux = v[-1][0]
+print (aux)
+
+#matriz
+
+m = [[2,4], [4,2]]
+
+# Recorrer Vector
+v = v [2:4]
+for x in v:
+    print(x)
+
+print("Digite el valor final de la sumatoria")
+a = int(input())
+suma = 0
+for x in range(a+1):
+    suma = suma + x
+print("La sumatoria es: ", suma) 
+###################
+print("Digite el valor final de la sumatoria")
+a = int(input())
+#suma = 0
+sumatoria = sum(range(a+1))
+print("La sumatoria es: ", sumatoria) 
+
+#while
+i = 1
+while i<5:
+    print(i)
+    i = i + 1
+  
+#Funciones - Metodos
+#Procedimiento no retorna valor
+    def hola_mundo():
+        print("Hola mundo")
+#Invocar el procedimiento        
+hola_mundo()
+
+# Funcion que retorna valor
+def elevar_cuadrado(numero):
+    return numero ** 2
+
+# Invocar función
+elevar_cuadrado(3)
+    
